dfde2bba8ab1aaca1102687b81997bae4fb715eb
Rick538 <68424088+Rick538@users.noreply.github.com>
2023-03-02 21:43:29 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,33 +4,35 @@
 #include <stdbool.h>
 #include <WiFi.h>
 #include <HTTPClient.h>
+#include <time.h>
 
 SHT3X sht30;
 QMP6988 qmp6988;
 
 const char* ssid = "Beranovi 2,4GHZ";
 const char* password = "Beran 58";
+const char* server = "api.thingspeak.com";
 const String apiKey = "YH8H47Z47TJYKD7Y";
-const String channelID = "2044897";
+const String channelId = "2044897";
 
-float nejnizsitlak = 0.0;
-float nejvyssitlak = 0.0;
-float aktualnitlak = 0.0;
-float rozdiltlaku = 0.0;
-float tolerancetlaku = 0.0;
-int hps = 0;
-int hpf = 0;
-int fontVelikost = 1.85;
-float zacatek = 0.0;
-float konec = 0.0;
-bool kali = true;
-bool start = false;
-bool middle = false;
-bool end = false;
-int drep = 8;
-float kalorie = 0.0;      
-float cas_za_drep = 0.0;   
-float cas_za_cviceni = 0.0;                                                       
+float NejnizsiTlak = 0.0;
+float NejvyssiTlak = 0.0;
+float AktualniTlak = 0.0;
+float RozdilTlaku = 0.0;
+float ToleranceTlaku = 0.0;
+int Hps = 0;
+int Hpf = 0;
+int FontVelikost = 1.85;
+bool Kali = true;
+bool Start = false;
+bool Middle = false;
+bool End = false;
+int Drep = 5;
+float Kalorie = 0.0;
+float CasZaDrep = 0.0;
+float CasZaCviceni = 0.0;
+int UplynulyCas;
+time_t start, current;
 
 void kalibrace()
 {
@@ -44,35 +46,35 @@ void kalibrace()
     M5.BtnA.read();
     if (M5.BtnA.isPressed())
     {
-      nejnizsitlak = qmp6988.calcPressure();
-      hpf = nejnizsitlak;
+      NejnizsiTlak = qmp6988.calcPressure();
+      Hpf = NejnizsiTlak;
     }
     M5.BtnB.read();
     if (M5.BtnB.isPressed())
     {   
-      nejvyssitlak = qmp6988.calcPressure();
-      hps = nejvyssitlak;
+      NejvyssiTlak = qmp6988.calcPressure();
+      Hps = NejvyssiTlak;
     }
-    if (nejnizsitlak >= nejvyssitlak)
+    if (NejnizsiTlak >= NejvyssiTlak)
     {
       continue;
     }
-    if(nejnizsitlak > 0||nejvyssitlak > 0)
+    if(NejnizsiTlak > 0|| NejvyssiTlak > 0)
     {
-      rozdiltlaku = hps - hpf;
-      if (rozdiltlaku < 0)
+      RozdilTlaku = Hps - Hpf;
+      if (RozdilTlaku < 0)
       {
-        rozdiltlaku * (-1);
-        tolerancetlaku = rozdiltlaku * 0.15;
-        round(tolerancetlaku);
-        start = true;
+        RozdilTlaku * (-1);
+        ToleranceTlaku = RozdilTlaku * 0.15;
+        round(ToleranceTlaku);
+        Start = true;
         return;
       }
-      if (rozdiltlaku > 0)
+      if (RozdilTlaku > 0)
       {
-        tolerancetlaku = rozdiltlaku * 0.15;
-        round(tolerancetlaku);
-        start = true;
+        ToleranceTlaku = RozdilTlaku * 0.15;
+        round(ToleranceTlaku);
+        Start = true;
         return;
       }
     }
@@ -86,27 +88,30 @@ void konec_hodnoty()
   M5.lcd.setCursor(0,5);
   M5.Lcd.printf("drepy: ");
   M5.lcd.setCursor(85,5);
-  M5.Lcd.println(drep);
+  M5.Lcd.println(Drep);
   M5.lcd.setCursor(0,50);
   M5.Lcd.printf("kalorie: ");
   M5.lcd.setCursor(105,50);
-  M5.Lcd.println(kalorie);
+  M5.Lcd.println(Kalorie);
   delay(5000);
 }
 void stats()
 {
   M5.lcd.setCursor(0,5);
-  M5.Lcd.printf("nejnizsi tlak: %.0f \n",nejnizsitlak);
+  M5.Lcd.printf("nejnizsi tlak: %.0f \n",NejnizsiTlak);
   M5.lcd.setCursor(0,15);
-  M5.Lcd.printf("nejvyssi tlak: %.0f \n",nejvyssitlak);
+  M5.Lcd.printf("nejvyssi tlak: %.0f \n",NejvyssiTlak);
   M5.lcd.setCursor(0,25);
-  M5.Lcd.printf("aktualni tlak: %.0f \n", aktualnitlak);
+  M5.Lcd.printf("aktualni tlak: %.0f \n", AktualniTlak);
   M5.lcd.setCursor(0,35);
-  M5.Lcd.printf("rozdil tlaku: %.0f \n", rozdiltlaku);
+  M5.Lcd.printf("rozdil tlaku: %.0f \n", RozdilTlaku);
   M5.lcd.setCursor(0,45);
-  M5.Lcd.printf("tolerance tlaku: %.0f \n", tolerancetlaku);
+  M5.Lcd.printf("tolerance tlaku: %.0f \n", ToleranceTlaku);
   M5.lcd.setCursor(0,65);
-  M5.Lcd.printf("cas: %.0f \n", cas_za_drep);
+  M5.Lcd.printf("cas: %.0f \n", CasZaDrep);
+  M5.lcd.setCursor(0,75);
+  M5.Lcd.printf("pocet drepu: %.0f \n", Drep);
+
 }
 
 WiFiClient client;
@@ -118,94 +123,92 @@ void setup() {
   Wire.begin(); //Wire init, adding the I2C bus.  Wire初始化, 加入i2c总线
   qmp6988.init();
   //M5.lcd.println(F("ENV Unit III test"));
-  M5.begin();
-  M5.Lcd.println("Connecting to Wi-Fi...");
+  M5.Lcd.printf("Connecting to Wi-Fi...");
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
-    M5.Lcd.print(".");
+    M5.Lcd.printf(".");
   }
-  M5.Lcd.print("");
-  M5.Lcd.print("Wi-Fi connected!");
+  M5.Lcd.printf("");
+  M5.Lcd.printf("Wi-Fi connected!");
 }
 void loop()
 {
-  M5.lcd.setTextSize(fontVelikost);
-  if(kali)
+  M5.lcd.setTextSize(FontVelikost);
+  if(Kali)
   {
     kalibrace();
-    kali = false;
+    Kali = false;
   }
   M5.Lcd.setTextColor(BLUE);
   M5.lcd.fillScreen(BLACK);
-  aktualnitlak = qmp6988.calcPressure();
+  AktualniTlak = qmp6988.calcPressure();
   stats();
-  Serial.println(tolerancetlaku);
-  if (nejnizsitlak + tolerancetlaku >= aktualnitlak)
+  Serial.println(ToleranceTlaku);
+  if (NejnizsiTlak + ToleranceTlaku >= AktualniTlak)
   {
-    if (start)
+    if (Start == true)
     {
-      zacatek = millis();
-      Serial.println(zacatek);
-      M5.lcd.setCursor(0,75);
+      M5.lcd.setCursor(0,85);
       M5.lcd.printf("jste na zacatku, následuje pohyb dolu");
-      middle = true;
+      Middle = true;
+      start = time(NULL);
     }
-    if (end)
+    if (End == true)
     {
-      konec = millis();
-      Serial.println(konec);
-      kalorie = kalorie + 0.5;
-      drep = drep + 1;
-      cas_za_drep = (konec - start)/1000;
-      Serial.println(cas_za_drep);
-      M5.lcd.setCursor(0,75);
-      cas_za_cviceni = cas_za_cviceni + cas_za_drep;
-      if (drep >= 10)
+      Kalorie = Kalorie + 0.5;
+      Drep ++;
+      M5.lcd.setCursor(0,85);
+      M5.lcd.printf("drep hotov pokracujte smer dolu");
+      if (Drep >= 10)
       {
-        M5.lcd.printf("jste na konci, udelal jste drep\r\n resetovali se hodnoty, muzete pokracovat");
-        String url = "http://api.thingspeak.com/update?api_key=" + apiKey + "&field1=" + String(cas_za_drep)+ "&field2=" + String(kalorie);
+        current = time(NULL);
+        UplynulyCas =(int) difftime(current, start);
+        M5.lcd.setCursor(0,85);
+        M5.lcd.printf("jste na konci celého cvičení\r\n resetovali se hodnoty, muzete pokracovat");
+        String url = "http://api.thingspeak.com/update?api_key=" + apiKey + "&field1=" + String(UplynulyCas) + "&field2=" + String(Kalorie) + "&field3=" + String(Drep);
         HTTPClient http;
         http.begin(client, url);
         int httpCode = http.GET();
-        if (httpCode > 0) {
+        if (httpCode > 0) 
+        {
           Serial.println("Data sent to ThingSpeak");
         } else {
           Serial.println("Error sending data to ThingSpeak");
         }
         http.end();
         konec_hodnoty();
-        drep = 0;
-        kalorie = 0;
-        nejnizsitlak = 0.0;
-        aktualnitlak = 0.0;
-        nejvyssitlak = 0.0;
+        Drep = 0;
+        Kalorie = 0;
+        NejnizsiTlak = 0.0;
+        AktualniTlak = 0.0;
+        NejvyssiTlak = 0.0;
         delay(2000);
         kalibrace();
       }
-      start = true;
-      middle = false; // toto je aby se předešlo nějakým problémům 
-      end = false; 
+      Start = true;
+      Middle = false; // toto je aby se předešlo nějakým problémům
+      End = false;
     }
   }
   else 
   {
-    M5.lcd.setCursor(0,60);
+    M5.lcd.setCursor(0,85);
     M5.Lcd.printf("jste v pohybu");
   }
-  if (nejvyssitlak - tolerancetlaku <= aktualnitlak)
+  if (NejvyssiTlak - ToleranceTlaku <= AktualniTlak)
   {
-    if(middle)
+    if (Middle == true)
     {
-      M5.lcd.setCursor(0,75);
+      M5.lcd.setCursor(0,85);
       M5.lcd.printf("jste uprosted drepu, následuje pohyb nahoru");
-      end = true;
-      start = false;
+      End = true;
+      Start = false;
     }
   }
   else
   {
-    M5.lcd.setCursor(0,60);
+    M5.lcd.setCursor(150,85);
     M5.Lcd.printf("jste v pohybu");
   }
   delay(800);
